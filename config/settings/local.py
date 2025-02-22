9e875556571035962f198c8ec8d2904cb80ca1d5
haruna <haruna.yuuki0712@gmail.com>
2025-02-22 22:15:32 +0900

    customize djoser

--- config/settings/local.py
+++ config/settings/local.py
@@ -24,6 +24,7 @@ ALLOWED_HOSTS = ["localhost","127.0.0.1", "0.0.0.0"]
 
 ADMIN_URL = getenv("DJANGO_ADMIN_URL")
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
 EMAIL_PORT = getenv("EMAIL_PORT")
 DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
 DOMAIN = getenv("DOMAIN")
--- config/urls.py
+++ config/urls.py
@@ -25,6 +25,8 @@ urlpatterns = [
         name="schema-redoc",
     ),
     path(settings.ADMIN_URL, admin.site.urls),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("core_apps.users.urls")),
 ]
 
 admin.site.site_header = "Alpha Apartments Admin"
