8183df3b5cfd270180510c36c4b2ae0d54f127f9
haruna <haruna.yuuki0712@gmail.com>
2025-02-23 01:30:12 +0900

    set up linter eslint font

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,10 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-6xl">Home Page</h1>
+      <h1 className="font-robotoSlab text-6xl">Home Page</h1>
+      <p className="font-OpenSans text-3xl">
+        brabravra
+      </p>
     </div>
   );
 }
