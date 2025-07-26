a30bb9a1ef61d5884bdc1c0ba0fca5261b233c8c
haruna <haruna.yuuki0712@gmail.com>
2025-07-26 13:27:29 +0900

    setup register page

--- client/src/app/(estate)/(auth)/register/page.tsx
+++ client/src/app/(estate)/(auth)/register/page.tsx
@@ -1,4 +1,5 @@
-import AuthFormHeader from "@/components/forms/auth/authFormHeader";
+import AuthFormHeader from "@/components/forms/auth/AuthFormHeader";
+import RegisterForm from "@/components/forms/auth/RegisterForm";
 
 export default function RegisterPage() {
 	return (
@@ -9,6 +10,17 @@ export default function RegisterPage() {
 				linkText="Login Here"
 				linkHref="/login"
 			/>
+			<div className="mt-7 sm:mx-auto sm:w-full sm:max-w-[480px]">
+				<div className="bg-lightGrey dark:bg-deepBlueGrey rounded-xl px-6 py-12 shadow sm:rounded-lg sm:px-12 md:rounded-3xl">
+					<RegisterForm />
+					{/* <div className="flex-center mt-5 space-x-2">
+						<div className="bg-richBlack dark:bg-platinum h-px flex-1"></div>
+						<span className="dark:text-platinum px-2 text-sm">Or</span>
+						<div className="bg-richBlack dark:bg-platinum h-px flex-1"></div>
+					</div>
+					<OauthButtons /> */}
+				</div>
+			</div>
 		</div>
 	);
 }
